1a4eb3bd1c653622cacd480a2bc460caf89ea404
Maria Stypsanelli <mstypsan.src@gmail.com>
2024-05-30 14:27:10 +0300

    Change the application port to 9000 due to conficts

--- apps/data-streams/src/main.ts
+++ apps/data-streams/src/main.ts
@@ -16,6 +16,6 @@ const initMicroservice = async (app: INestApplication) => {
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await initMicroservice(app);
-  await app.listen(3000);
+  await app.listen(9000);
 }
 bootstrap();
