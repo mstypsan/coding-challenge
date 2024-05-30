b0e55ac8b961b22bfed5b48469b2476f6a6af74b
Maria Stypsanelli <mstypsan.src@gmail.com>
2024-05-30 14:24:04 +0300

    Create an ApiService and delete the pre-existing AppService

--- apps/data-streams/src/app.module.ts
+++ apps/data-streams/src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ApiController } from './api/api.controller';
+import { ApiService } from './api/api.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [ApiController],
+  providers: [ApiService],
 })
 export class AppModule {}
