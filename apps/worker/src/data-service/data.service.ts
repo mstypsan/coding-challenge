b7cae3c74a30d318d7e24f2af63e10060f4f7968
Maria Stypsanelli <mstypsan.src@gmail.com>
2024-05-31 13:41:23 +0300

    In case of an error, return undefined so that we don't emit the event

--- apps/worker/src/data-service/data.service.ts
+++ apps/worker/src/data-service/data.service.ts
@@ -1,7 +1,7 @@
 import { HttpService } from '@nestjs/axios';
 import { Inject, Injectable } from '@nestjs/common';
 import { AxiosError, AxiosResponse } from 'axios';
-import { Observable, catchError, firstValueFrom } from 'rxjs';
+import { Observable, catchError, firstValueFrom, of } from 'rxjs';
 import { ClientProxy } from '@nestjs/microservices';
 import { CatData } from '@app/shared';
 
@@ -14,19 +14,22 @@ export class DataService {
 
   async processData(): Promise<void> {
     const catData = await this.getData();
+    if (!catData) {
+      return;
+    }
     this.client.emit('cat_data_received', catData);
   }
 
-  private async getData(): Promise<CatData> {
+  private async getData(): Promise<CatData | undefined> {
     const response = await firstValueFrom<AxiosResponse<CatData>>(
       this.httpService.get('https://catfact.ninja/fact').pipe(
         catchError((error: AxiosError, caught: Observable<any>) => {
           //log error
           //implement exponential backoff
-          return caught;
+          return of(undefined);
         }),
       ),
     );
-    return response.data;
+    return response?.data;
   }
 }
